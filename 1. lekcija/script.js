f3ae23dd3be5d3477a56e98ea8ef21201ae9f8ce
cvijander <cvija85@gmail.com>
2024-08-05 16:17:09 +0200

    Declaring and assigning variables

---
+++ 1. lekcija/script.js
@@ -0,0 +1,13 @@
+
+var x =32;
+
+console.log(x);
+console.log("The x is " +typeof x);
+
+var whereAmI = "Santa Barbara, CA";
+
+whereAmI = "Los Angeles, CA";
+
+whereAmI =75; 
+
+var monster1="Grover", monster2='Cookie', monster3 ='Animal';
