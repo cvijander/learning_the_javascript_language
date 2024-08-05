8b0986dfda1a6e7a5447120179128b58e33aee55
cvijander <cvija85@gmail.com>
2024-08-05 18:00:16 +0200

    Variable, true, false, let, const, var

---
+++ 2. lekcija strings/script.js
@@ -0,0 +1,38 @@
+
+var s = 'this is a string';
+var t ="this is a string";
+
+var num12='12';
+
+// var m='this is a string"; error
+
+var anchor = '<a href="">';
+
+var an = '<a href="https">';
+
+var m = "This is Joe's favourite string";
+m = "This is Joe's \"favourite\" string";
+
+var declaration =`This i say to you : "${m}" something`;
+console.log(declaration); 
+
+var mnu =true;
+mnu =false;
+var buttonCliked = false;
+
+var myLocation="Santa Brabara";
+var myOtherLocation="Los Angeles";
+myLocation ===myOtherLocation;
+console.log(myLocation===myOtherLocation);
+myOtherLocation="Santa Brabara";
+console.log(myLocation === myOtherLocation);
+
+const dozen =12;
+
+
+
+
+
+// m= "This is \n 
+//     Joe's Favourite \n
+//     String EVER";
